245e5b0e4c503ac4d82ef386c442f4db0c27d25a
arsimr16 <arsimr16@g.holycross.edu>
2018-01-18 14:08:01 -0800

    update webpack config and create app component

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,25 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const ce = React.createElement;
+const App = () => (
+	<div className="app">
+		<div className="landing">
+			<h1>svideo</h1>
+			<input type="text" placeholder="search" />
+			<a>Or Browse All</a>
+		</div>
+	</div>
+);
 
-const MyTitle = function(props) {
-  return (
-  	<div>
-  		<h1 style={ { color: props.color } }>{props.title}</h1>
-  	</div>
-  );
-};
-
-const MyFirstComponent = function() {
-  return (
-  	<div id="my-first-component">
-  		<MyTitle title="Game of Thrones" color="YellowGreen"/>
-  		<MyTitle title="Stranger Things" color="GreenYellow"/>
-  		<MyTitle title="Rick and Morty" color="LimeGreen"/>
-  		<MyTitle title="House of Cards" color="peru"/>
-  	</div>
-  );
-};
-
-render(ce(MyFirstComponent), document.getElementById('app'));
+render(<App />, document.getElementById('app'));
